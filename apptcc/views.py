e0e796822b9589014c3403730db8c41f31d306e4
AndreTavaress <96271747+AndreTavaress@users.noreply.github.com>
2024-04-25 17:45:09 -0300

    Update-documentação

--- apptcc/views.py
+++ apptcc/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render, redirect
-from .models import Dispositivo
+from .models import Dispositivo, Offline
 
 def documentacao(request): 
-    numeros = range(1, 101)
-    link_base = '/admin/apptcc/dispositivo/add/?numero='  # Adicionando o marcador de posição para o número
-    return render(request, 'documentacao.html', {'numeros': numeros, 'link_base': link_base})
-    
-def adicionar_dispositivo(request):
-    numero = request.GET.get('numero')
-    if numero:
-        dispositivo = Dispositivo.objects.create(ip=numero)
-        # Redirecionar para a página de adição de dispositivo, passando o número como parâmetro
-        return redirect('/admin/apptcc/dispositivo/add/?numero=' + numero)
+#    numeros = range(1, 101)
+#    link_base = '/admin/apptcc/dispositivo/add/?numero='  # Adicionando o marcador de posição para o número
+#    return render(request, 'documentacao.html', {'numeros': numeros, 'link_base': link_base})
+    endereco = Offline.objects.all()
+    return render(request, 'documentacao.html', {'endereco_ip' : endereco})
     
 def home(request):
     return render(request, 'home.html')
